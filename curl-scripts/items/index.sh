1aabd10455ab61670874dc83fc342e518b6c5599
Ashley Norwood <norwood54@icloud.com>
2020-07-16 14:26:17 -0400

    Committing to get my brach clean for pull

--- curl-scripts/items/index.sh
+++ curl-scripts/items/index.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 API="http://localhost:4741"
-URL_PATH="/list-items"
+URL_PATH="/lists/${LIST_ID}/items"
 
 curl "${API}${URL_PATH}" \
   --include \
   --request GET \
-  --header "Authorization: Bearer ${TOKEN}"
+  --header "Authorization: Bearer ${TOKEN}" \
 
 echo
--- curl-scripts/items/show.sh
+++ curl-scripts/items/show.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 API="http://localhost:4741"
-URL_PATH="/list-items"
+URL_PATH="/lists/${LIST_ID}/items/${ITEM_ID}"
 
-curl "${API}${URL_PATH}/${ID}" \
+curl "${API}${URL_PATH}/" \
   --include \
   --request GET \
   --header "Authorization: Bearer ${TOKEN}"
