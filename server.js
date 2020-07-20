4f9a1a29b282b8475f6cf9d70b60b56e3b035fed
Ashley Norwood <norwood54@icloud.com>
2020-07-20 18:06:24 -0400

    Removed/commented out all console.log statements

--- server.js
+++ server.js
@@ -75,7 +75,7 @@ app.use(errorHandler)
 
 // run API on designated port (4741 in this case)
 app.listen(port, () => {
-  console.log('listening on port ' + port)
+  // console.log('listening on port ' + port)
 })
 
 // needed for testing
